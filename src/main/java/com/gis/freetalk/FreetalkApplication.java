f1c10bdc82e305c43795de133f72b53fae2cd222
yuhuachang-pc <1571325709@qq.com>
2019-12-17 22:57:53 +0800

    init

---
+++ src/main/java/com/gis/freetalk/FreetalkApplication.java
@@ -0,0 +1,13 @@
+package com.gis.freetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreetalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gis/freetalk/FreetalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.freetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreetalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
