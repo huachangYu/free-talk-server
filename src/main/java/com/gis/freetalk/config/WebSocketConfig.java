3d3d81da106885e6b20df77e8f6dba9540cd49f0
yuhuachang-pc <1571325709@qq.com>
2019-12-19 00:03:57 +0800

    learn vuex

--- src/main/java/com/gis/freetalk/config/WebSocketConfig.java
+++ src/main/java/com/gis/freetalk/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        
+
     }
 }
